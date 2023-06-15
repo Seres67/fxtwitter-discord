83536b7a86e05092f61dc46bb16e60819f8d6c75
Laurent <haarlaurent67@hotmail.fr>
2023-06-15 20:19:21 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use discord::model::Event;
+use discord::Discord;
+
+fn main() {
+    let discord = Discord::from_bot_token(
+        "MTExODk2NTc4NTM0ODUzODUwOA.GTqGkc.WyEDZrhyw6zn50Iq1w33eRb--qB07CMfQ_A7cI",
+    )
+    .expect("Wrong token");
+    let (mut connection, _) = discord.connect().expect("connection failed");
+
+    loop {
+        match connection.recv_event() {
+            Ok(Event::MessageCreate(message)) => {
+                if message.content.contains("https://twitter.com") {
+                    let reply = message.content.replace("twitter.com", "fxtwitter.com");
+                    let _ = discord.delete_message(message.channel_id, message.id);
+                    let _ = discord.send_message(message.channel_id, &format!("{} sent {reply}", message.author.mention()), "", false);
+                }
+            }
+            Ok(_) => {}
+            Err(discord::Error::Closed(code, body)) => {
+                println!("connection closed {code:?}: {body}");
+            }
+            Err(err) => println!("Error received {err}"),
+        }
+    }
+}
